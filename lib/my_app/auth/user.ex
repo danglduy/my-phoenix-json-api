afb2bdd9b46a387d15304023bb6d18322bbf7db3
danglduy <38782010+danglduy@users.noreply.github.com>
2019-12-25 23:51:29 +0700

    Remove is_active column

--- lib/my_app/auth/user.ex
+++ lib/my_app/auth/user.ex
@@ -6,7 +6,6 @@ defmodule MyApp.Auth.User do
   @foreign_key_type :binary_id
   schema "users" do
     field :email, :string
-    field :is_active, :boolean, default: false
     field :password, :string, virtual: true
     field :password_hash, :string
 
@@ -18,8 +17,8 @@ defmodule MyApp.Auth.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:email, :is_active, :password])
-    |> validate_required([:email, :is_active, :password])
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
     |> unique_constraint(:email)
     |> put_password_hash()
   end
